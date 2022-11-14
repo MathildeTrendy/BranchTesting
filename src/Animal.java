5973a4cd33b0f4290b352241d04fabd91f6a123d
mathildetrend <math38v6@stud.kea.dk>
2022-11-14 13:38:00 +0100

    Main tilføjet

--- src/Animal.java
+++ src/Animal.java
@@ -1,6 +1,12 @@
-abstract class Animal {
+public abstract class Animal {
 
     private String name;
+
+   public abstract void makeSound();
+
+    public abstract boolean eat(String foodType);
+
+
 
 
 
